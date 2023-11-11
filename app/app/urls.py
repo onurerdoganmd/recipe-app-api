7ff84bfdc6d033b38b49b9e091c1d21c837af858
onurerdoganmd <onurerdogan037@gmail.com>
2023-11-11 22:40:31 +0300

    Started building user API

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from drf_spectacular.views import(SpectacularAPIView,
-    SpectacularSwaggerView,
-)
+                                  SpectacularSwaggerView)
 from django.contrib import admin
 from django.urls import path, include
 
